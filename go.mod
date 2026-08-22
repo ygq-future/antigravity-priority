21363ca5fc34a4d8e0ca99fab4b91b07a69784ab
sheepyu <3029944576@qq.com>
2026-08-22 20:46:19 +0800

    chore(release): prepare v1.2.2

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module antigravity-priority
 
-go 1.22
+go 1.26.7
--- internal/management/feature_shell_assets.go
+++ internal/management/feature_shell_assets.go
@@ -10,7 +10,7 @@ const managementPageShellMarkup = `
             <div class="brand-zone">
                 <h1>
                     <span data-i18n="title">Antigravity Priority</span>
-                    <span class="version-badge">v1.2.1</span>
+                    <span class="version-badge">v1.2.2</span>
                 </h1>
             </div>
             <div class="topbar-actions">
